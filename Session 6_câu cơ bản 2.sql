0a0d9492c271b5c5f3c521c69eefa83acb5268c4
HN_KS25_QTKD1_Đoàn Quỳnh Anh <doanquynhanh150675@gmail.com>
2025-11-14 20:00:13 +0700

    Add files via upload

---
+++ Session 6_câu cơ bản 2.sql
@@ -0,0 +1,12 @@
+-- 1. Chèn dữ liệu vào bảng 
+insert into TacGia 
+values('Nguyễn Nhật Ánh', 'Việt Nam'),
+('J.K. Rowling', 'Anh'),
+('Haruki Murakami', 'Nhật Bản'),
+('Dale Carnegie', 'Mỹ');
+insert into DocGia
+values ('Nguyễn Văn A', '123 Đường ABC, Hà Nội', '0901234567'),
+('Trần Thị B', '456 Đường XYZ, TP.HCM', '0912345678'),
+('Lê Văn C', '789 Đường LMN, Đà Nẵng', '0923456789'),
+('Phạm Thị D', '101 Đường QRS, Hà Nội', '0934567890'),
+('Hoàng Văn E', '202 Đường UVW, Cần Thơ', '0945678901')
